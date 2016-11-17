8d34483710e71e4abe49f5618c9b45e2338deb29
grossmanm <grossmanm@carleton.edu>
2016-11-17 14:08:19 -0600

    Update euchre.py

--- euchre.py
+++ euchre.py
@@ -6,7 +6,7 @@
 import random 
 #import graphics 
 class Cards:
-
+    # this class creates cards. Each card has a number associated to its number and suit.
     def __init__(self, suit, number):
         # suits are defined by numbers 0-4. Spades = 0, Clubs = 1, Hearts = 2, Diamonds = 3, Jokers = 4 and 5
         self.suit = suit
@@ -27,7 +27,7 @@ class Cards:
     
     
 class Deck:
-    
+    # this class creates a deck containing cards.
     def __init__(self, game_type):
         self.game_type = game_type
         deck = []
@@ -46,32 +46,38 @@ class Deck:
     
     def get_deck(self):
         return self.deck
+    
     def choose_cards(self):
+        # this method determines how many cards will be played with in this game.
+        # redefines self.deck so that it reflects the deck that will be used in the game.
         self.game_deck = []
         if self.game_type == 34:
             for k in self.deck:
                 if k.get_number() >= 7:
                     self.game_deck.append(k)
-            return self.game_deck
+            self.deck = self.game_deck
         
         elif self.game_type == 30:
             for k in self.deck:
                 if k.get_number() >= 8:
                     self.game_deck.append(k)
-            return self.game_deck
+            self.deck = self.game_deck
         
         elif self.game_type == 26:
             for k in self.deck:
                 if k.get_number() >= 9:
                     self.game_deck.append(k)
-            return self.game_deck
+            self.deck = self.game_deck
+        return self.deck
         
     def shuffle(self):
+        # this method shuffles the deck.
         self.shuffled_deck= []
         for card in self.game_deck:
             n = random.randrange(len(self.game_deck))
             self.shuffled_deck.insert(n, card)
-        return self.shuffled_deck
+        self.deck = self.shuffled_deck
+        return self.deck
 
     def deal(self):
         pass
@@ -79,19 +85,78 @@ class Deck:
         pass
 
         
-
+class Trick:
+    # this class will be used to play each round of Eucher, called tricks.
+    def __init__(self, game_type, num_players, player0, player1, player2 = None, player3 = None):
+        self.game_type = game_type
+        self.num_players = num_players
+        self.deck = Deck(self.game_type)
+        self.player0 = player0
+        self.player1 = player1
+        self.player2 = player2
+        self.player3 = player3
+        self.deck.choose_cards()
+        self.deck.shuffle()
+        # use self.deck.deck to access the list of cards
         
+    def getGameType(self):
+        return self.game_type
+    
+    def getNumPlayers(self):
+        return self.num_players
+    
+    def getDeck(self):
+        return self.deck
+    
+    def deal(self, player):
+        for i in range(5):
+            player.hand.append(self.deck.deck.pop())
+        return player.hand
+        
+class Player:
+    # creates a player that can either be controlled by the player or the AI.
+    def __init__(self, control, score):
+        self.control = control
+        self.score = score
+        self.hand = []
+        
+    def getControl(self):
+        return self.control
+    
+    def getHand(self):
+        return self.hand
+    
+    def getScore(self):
+        return self.score
+    
+def play_game():
+    # plays the game
+    cardChoice= input("How many cards do you want to play with? 34, 30, 26 ")
+    cardChoice= int(cardChoice)
+    num_players= input("How many players do you want to play with? 2, 3, 4 ")
+    num_players= int(num_players)
+    if num_players == 4:
+        player0 = Player(None)
+        player1 = Player(None)
+        player2 = Player(None)
+        player3 = Player(None)
+    elif num_players == 3:
+        player0 = Player(None)
+        player1 = Player(None)
+        player2 = Player(None)
+    elif num_players == 2:
+        player0 = Player(None)
+        player1 = Player(None)
+    while player0.score < 10 or player1.score < 10 or player2.score < 10 or player3.score < 10:
+        t = Trick(cardChoice, num_players, player0, player1, None, None)
+    
 """       
 for j in range(4):
     for i in range(14):
         Cards(j, i+2)
 """
 def main():
-    cardChoice= input("How many cards do you want to play with? 34, 30, 26 ")
-    cardChoice= int(cardChoice)
-    t = Deck(cardChoice)
-    t.choose_cards()
-    t.shuffle()
+    play_game()
     
     
 if __name__ == "__main__":
